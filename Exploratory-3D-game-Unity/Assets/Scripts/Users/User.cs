c08c9a75e6cd7d99b47f9f8bb55e94955e479b78
MajaI <maja.isakov@gmail.com>
2016-08-15 23:43:53 +0200

    Created User class and started implementation of PlayerPrefsUserList

---
+++ Exploratory-3D-game-Unity/Assets/Scripts/Users/User.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace zm.Users
+{
+    public class User
+    {
+
+        #region Fields and Properties
+
+        /// <summary>
+        /// Amount of points that user collected.
+        /// </summary>
+        public int Points { get; private set; }
+
+        /// <summary>
+        /// User name that will be displayed during the game.
+        /// </summary>
+        public string Name { get; private set; }
+
+        #endregion Fields and Properties
+
+        #region Constructor
+
+        public User(string name, int points = 0)
+        {
+            this.Name = name;
+            this.Points = points;
+        }
+
+        #endregion Constructor
+
+    }
+}
