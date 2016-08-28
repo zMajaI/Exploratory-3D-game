37933d8c4cbb93e9d7d028981c1258557b2993d2
MajaI <maja.isakov@gmail.com>
2016-08-28 10:18:50 +0200

    Added test for colliders on the Level scene + some random generated metas

---
+++ Exploratory-3D-game-Unity/Assets/Scripts/Levels/View/Components/QuestionableObjectView.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using zm.Questioning;
+
+namespace zm.Levels
+{
+    public class QuestionableObjectView : MonoBehaviour
+    {
+        #region Fields and Properties
+        [SerializeField]
+        private bool rotate;
+
+        [SerializeField]
+        private float rotateSpeed = 50f;
+
+        [SerializeField]
+        private GameObject cube;
+
+        #endregion Fields and Properties
+
+        #region MonoBehaviour Methods
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rotate)
+            {
+                cube.transform.Rotate(new Vector3(0f, Time.deltaTime * rotateSpeed, 0f));
+            }
+            else
+            {
+                cube.transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            rotate = true;
+            Debug.Log("<color=magenta> T Enter <color>");
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            rotate = false;
+            Debug.Log("<color=magenta> T Exit <color>");
+        }
+
+        #endregion MonoBehaviour Methods
+
+        public void OnCollisionExit(Collision collision)
+        {
+            Debug.Log("<color=magenta>  Enter <color>");
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("<color=magenta> Enter <color>");
+        }
+
+        #region Public Methods
+
+        public void Initialize(Question question)
+        {
+
+        }
+
+        #endregion Public Methods
+    }
+}
