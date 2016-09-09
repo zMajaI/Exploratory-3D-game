deb1bd1e6c2814ef65e2128e9c9c1b3d7f4b9644
MajaI <maja.isakov@gmail.com>
2016-09-09 21:36:42 +0200

    Added new wand and level designe

--- Exploratory-3D-game-Unity/Assets/Scripts/Util/AnimationHelper.cs
+++ Exploratory-3D-game-Unity/Assets/Scripts/Util/AnimationHelper.cs
@@ -1,126 +1,117 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace zm.Util
+public class AnimationHelper : MonoBehaviour
 {
-    public class AnimationHelper : MonoBehaviour
+
+    #region Fields and Properties
+
+    [SerializeField]
+    private bool rotateX;
+
+    [SerializeField]
+    private bool rotateY;
+
+    [SerializeField]
+    private bool rotateZ;
+
+    [SerializeField]
+    private float rotateSpeed = 50f;
+
+    [SerializeField]
+    private float offsetX;
+
+    [SerializeField]
+    private float offsetY;
+
+    [SerializeField]
+    private float offsetZ;
+
+    [SerializeField]
+    private float translateSpeed = 0.1f;
+
+    private Vector3 initialPosition;
+
+    #endregion Fields and Properties
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPosition = transform.position;
+        startPostion = endPosition = transform.position;
+    }
+
+    private Vector3 endPosition;
+    private Vector3 startPostion;
+    private bool direction = true;
+
+    void LateUpdate()
     {
 
-        #region Fields and Properties
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(rotateX ? Time.deltaTime * rotateSpeed : 0f, rotateY ? Time.deltaTime * rotateSpeed : 0f, rotateZ ? Time.deltaTime * rotateSpeed : 0f));
 
-        [SerializeField]
-        private bool rotateX;
+        Vector3 maxPosition = initialPosition + new Vector3(offsetX, offsetY, offsetZ);
+        Vector3 minPosition = initialPosition - new Vector3(offsetX, offsetY, offsetZ);
+        bool reachedMax = (offsetX > 0f && transform.position.x - maxPosition.x >= -0.01f) || (offsetY > 0f && transform.position.y - maxPosition.y >= -0.01f) || (offsetZ > 0f && transform.position.z - maxPosition.z >= -0.01f);
+        bool reachedMin = (offsetX > 0f && transform.position.x - minPosition.x <= 0.01f) || (offsetY > 0f && transform.position.y - minPosition.y <= 0.01f) || (offsetZ > 0f && transform.position.z - minPosition.z <= 0.01f);
 
-        [SerializeField]
-        private bool rotateY;
-
-        [SerializeField]
-        private bool rotateZ;
-
-        [SerializeField]
-        private float rotateSpeed = 50f;
-
-        [SerializeField]
-        private float offsetX;
-
-        [SerializeField]
-        private float offsetY;
-
-        [SerializeField]
-        private float offsetZ;
-
-        [SerializeField]
-        private float translateSpeed = 0.1f;
-
-        private Vector3 initialPosition;
-
-        /// <summary>
-        /// Flag indicating if animation should be played.
-        /// </summary>
-        public bool Play = true;
-
-        #endregion Fields and Properties
-
-        #region MonoBehaviour Methods
-        // Use this for initialization
-        void Start()
+        if (!direction && reachedMax)
         {
-            initialPosition = transform.position;
-            endPosition = transform.position;
+            direction = true;
         }
-
-        private Vector3 endPosition;
-        private bool direction = true;
-
-        // Update is called once per frame
-        void Update()
+        else if (reachedMin)
         {
-            // If not enabled just return - do not play animation
-            if (!Play) return;
+            direction = false;
+        }
+        
 
-            gameObject.transform.Rotate(new Vector3(rotateX ? Time.deltaTime * rotateSpeed : 0f, rotateY ? Time.deltaTime * rotateSpeed : 0f, rotateZ ? Time.deltaTime * rotateSpeed : 0f));
-
-            Vector3 maxPosition = initialPosition + new Vector3(offsetX, offsetY, offsetZ);
-            Vector3 minPosition = initialPosition - new Vector3(offsetX, offsetY, offsetZ);
-            bool reachedMax = (offsetX > 0f && transform.position.x - maxPosition.x >= -0.01f) || (offsetY > 0f && transform.position.y - maxPosition.y >= -0.01f) || (offsetZ > 0f && transform.position.z - maxPosition.z >= -0.01f);
-            bool reachedMin = (offsetX > 0f && transform.position.x - minPosition.x <= 0.01f) || (offsetY > 0f && transform.position.y - minPosition.y <= 0.01f) || (offsetZ > 0f && transform.position.z - minPosition.z <= 0.01f);
-
-            if (!direction && reachedMax)
+        if (offsetX > 0f)
+        {
+            if (reachedMax || direction)
             {
-                direction = true;
+                endPosition.Set(minPosition.x, initialPosition.y, initialPosition.z);
             }
-            else if (reachedMin)
+            else if (!direction)
             {
-                direction = false;
+                endPosition.Set(maxPosition.x, initialPosition.y, initialPosition.z);
             }
-
-
-            if (offsetX > 0f)
-            {
-                if (reachedMax || direction)
-                {
-                    endPosition.Set(minPosition.x, initialPosition.y, initialPosition.z);
-                }
-                else if (!direction)
-                {
-                    endPosition.Set(maxPosition.x, initialPosition.y, initialPosition.z);
-                }
-
-            }
-
-            if (offsetY > 0)
-            {
-                if (reachedMax || direction)
-                {
-                    endPosition.Set(endPosition.x, minPosition.y, initialPosition.z);
-                }
-                else if (!direction)
-                {
-                    endPosition.Set(endPosition.x, maxPosition.y, initialPosition.z);
-                }
-            }
-
-            if (offsetZ > 0)
-            {
-                if (reachedMax || direction)
-                {
-                    endPosition.Set(endPosition.x, endPosition.y, minPosition.z);
-                }
-                else if (!direction)
-                {
-                    endPosition.Set(endPosition.x, endPosition.y, maxPosition.z);
-                }
-            }
-
-            if (endPosition != initialPosition)
-            {
-
-                transform.position = Vector3.MoveTowards(transform.position, endPosition, Time.deltaTime * translateSpeed); //Vector3.Lerp(transform.position, endPosition, Time.deltaTime * translateSpeed);
-            }
-
 
         }
 
-        #endregion MonoBehaviour Methods
+        if (offsetY > 0)
+        {
+            if (reachedMax || direction)
+            {
+                endPosition.Set(endPosition.x, minPosition.y, initialPosition.z);
+            }
+            else if (!direction)
+            {
+                endPosition.Set(endPosition.x, maxPosition.y, initialPosition.z);
+            }
+        }
+
+        if (offsetZ > 0)
+        {
+            if (reachedMax || direction)
+            {
+                endPosition.Set(endPosition.x, endPosition.y, minPosition.z);
+            }
+            else if (!direction)
+            {
+                endPosition.Set(endPosition.x, endPosition.y, maxPosition.z);
+            }
+        }
+
+        if (endPosition != initialPosition)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, Time.deltaTime * translateSpeed); //Vector3.Lerp(transform.position, endPosition, Time.deltaTime * translateSpeed);
+        }
+
+
     }
 }
